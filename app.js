505b1fe50f483b27fb5bb7c2133af30592bd45bf
Mats <mats.weissenberg@gmail.com>
2026-01-04 17:23:19 +0200

    Fallback for react router

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// React Router fallback
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
--- playwright.config.js
+++ playwright.config.js
@@ -40,6 +40,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -49,6 +50,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+    */
 
     /* Test against mobile viewports. */
     // {
